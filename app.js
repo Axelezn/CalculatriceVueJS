462abb53e363633a90571ae32039563c369b8661
Axel Eouzan <axeleouzan@gmail.com>
2024-01-24 15:28:02 +0100

    Commit MAJ jour 2

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
 new Vue({
+  // Déclaration de l'instance Vue
   el: "#app",
-  data: {},
+  data: {
+    // Data
+    answer: "",
+    current: "",
+    clicked: true,
+    log: "",
+  },
+  methods: {
+    BtnClick(value) {
+      if (this.clicked) {
+        this.current = "";
+        this.clicked = false;
+      }
+      this.current = `${this.current}${value}`;
+    },
+    clear() {
+      this.answer = "";
+      this.current = "";
+      this.answer = "";
+      this.clicked = true;
+      this.log = "";
+    },
+    changeSigne() {
+      if (this.current !== "") {
+        if (this.current.charAt(0) === "-") {
+          this.current = this.current.slice(1);
+        } else {
+          this.current = `-${this.current}`;
+        }
+      }
+    },
+    toPercent() {
+      if (this.current != "") {
+        this.current = `${parseFloat(this.current) / 100}`;
+      }
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.current = `${this.current}.`;
+      }
+    },
+  },
 });
